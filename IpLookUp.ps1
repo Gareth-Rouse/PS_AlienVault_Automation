424e7281b34534c1e4d9188dc304400f3e59ecaa
Gareth Rouse <46309432+Gareth-Rouse@users.noreply.github.com>
2020-02-19 22:55:28 +0000

    Create IpLookUp.ps1

---
+++ IpLookUp.ps1
@@ -0,0 +1,6 @@
+$a = Get-Clipboard
+start https://www.shodan.io/host/$a
+##start http://whois.domaintools.com/$a
+start https://www.virustotal.com/#/ip-address/$a
+start https://urlscan.io/ip/$a
+start https://censys.io/ipv4/$a
