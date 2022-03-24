24c76753917c147ac1605be0f15e56e4659b7b9a
Gareth Rouse <46309432+Gareth-Rouse@users.noreply.github.com>
2022-03-24 16:46:18 +0000

    Update LockOut.ps1

--- LockOut.ps1
+++ LockOut.ps1
@@ -2,5 +2,5 @@ param($minutes = 500)
 $myshell = New-Object -com "Wscript.Shell"
 for ($i = 0; $i -lt $minutes; $i++) {
   Start-Sleep -Seconds 60
-  $myshell.sendkeys({F13})
+  $myshell.sendkeys("{F13}")
 }
